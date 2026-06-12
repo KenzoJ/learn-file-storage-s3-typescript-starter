50e1248b2564a3fb3c76c3e9a70467ba392c29db
KenzoJ <charles.felux@gmail.com>
2026-06-12 14:21:53 -0400

    refact: add frontend cache buster for thumbnail upload

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,12 +1,10 @@
 import { getBearerToken, validateJWT } from "../auth";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { Buffer } from 'node:buffer'
-import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
-
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -30,30 +28,30 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!(file instanceof File)) {
     throw new BadRequestError("Thumbnail file missing");
   }
+
   const MAX_UPLOAD_SIZE = 10 << 20;
 
   if (file.size > MAX_UPLOAD_SIZE) {
-    throw new BadRequestError("File size too large")
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`,
+    );
   }
+
   const mediaType = file.type;
-  if (!mediaType) {
-    throw new BadRequestError("Missing Content-Type for thumbnail");
-  }
   if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
-    throw new BadRequestError("Needs to be png or jpeg");
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
   }
 
-
-  const ext = mediaTypeToExt(mediaType)
+  const ext = mediaTypeToExt(mediaType);
   const filename = `${videoId}${ext}`;
 
-  const assetDiskPath = getAssetDiskPath(cfg, filename)
-  await Bun.write(assetDiskPath, file)
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
 
   const urlPath = getAssetURL(cfg, filename);
   video.thumbnailURL = urlPath;
-
-  updateVideo(cfg.db, video)
+  updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
 }
+
